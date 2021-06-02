5f6e1d2de57bf5dba967c1d377241c6ae42f372a
filip <filipwlodarczyk99@gmail.com>
2021-06-02 12:07:05 +0200

    ---

--- src/main/java/pl/filipwlodarczyk/RegistrationApplication/AppUser/AppUserService.java
+++ src/main/java/pl/filipwlodarczyk/RegistrationApplication/AppUser/AppUserService.java
@@ -1,25 +1,34 @@
 package pl.filipwlodarczyk.RegistrationApplication.AppUser;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.filipwlodarczyk.RegistrationApplication.registration.Token.ConfirmationToken;
+import pl.filipwlodarczyk.RegistrationApplication.registration.Token.ConfirmationTokenService;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @Transactional
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
 private final AppUserRepository appUserRepository;
 private final BCryptPasswordEncoder bCryptPasswordEncoder;
+private final ConfirmationTokenService confirmationTokenService;
 
 private final static String USER_NOT_FOUND_MSG = "Nie znaleziono użytkownika z taki emailem %s";
 
-    public AppUserService(AppUserRepository appUserRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.appUserRepository = appUserRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
+//    public AppUserService(AppUserRepository appUserRepository, BCryptPasswordEncoder bCryptPasswordEncoder, ConfirmationTokenService confirmationTokenService) {
+//        this.appUserRepository = appUserRepository;
+//        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+//        this.confirmationTokenService = confirmationTokenService;
+//    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -40,6 +49,14 @@ private final static String USER_NOT_FOUND_MSG = "Nie znaleziono użytkownika z 
         appUser.setPassword(encodedPassword);
 
         appUserRepository.save(appUser);
-        return "";
+
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),appUser
+        );
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+        return token;
     }
 }
---
+++ src/main/java/pl/filipwlodarczyk/RegistrationApplication/registration/Token/ConfirmationTokenService.java
@@ -0,0 +1,15 @@
+package pl.filipwlodarczyk.RegistrationApplication.registration.Token;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken confirmationToken){
+        confirmationTokenRepository.save(confirmationToken);
+    }
+}
